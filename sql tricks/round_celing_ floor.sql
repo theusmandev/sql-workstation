76368eca9fb5b8a7a2c61b0d38a7785109c5a716
theusmandev <theusmandeveloper@gmail.com>
2025-10-24 15:52:54 +0500

    round,floor,celing added

---
+++ sql tricks/round_celing_ floor.sql
@@ -0,0 +1,20 @@
+--trick no. 01
+
+
+
+declare @var float = 57.89
+select round(@var,1)
+
+
+declare @var varchar(40) = 57.89
+select round(@var,1)
+
+
+
+
+select ceiling(@var)
+select floor()
+
+
+DECLARE @x DECIMAL(5,2) = 12.45777;
+SELECT @x AS Value;
